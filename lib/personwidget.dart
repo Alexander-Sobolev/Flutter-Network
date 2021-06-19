41242451d7db278a1c9194fb3ee539d16c7faa62
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-06-19 17:58:05 +0300

    loader

---
+++ lib/personwidget.dart
@@ -0,0 +1,17 @@
+import 'package:network_flutter/personloader.dart';
+
+class PersonDetailsPage
+
+
+
+class State extends State<PersonDetailsPage> {
+
+}
+
+
+
+void loadData() async {
+  var personInfo = await loadPerson(id);
+
+
+}
